5f044c2b7c7564fc5e343d2dbff48ab88e1e0b2a
Roy Tan <roy.tan@gmail.com>
2023-01-10 23:44:17 +0100

    Write JSON output file

--- routes/push.js
+++ routes/push.js
@@ -1,15 +1,30 @@
 var express = require('express');
 var router = express.Router();
+const fs = require('fs');
 
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   //res.send('respond with a resource');
   res.setHeader('Content-Type', 'application/json');
-  res.end(JSON.stringify({ a: 1 }, null, 3));
+  res.end(JSON.stringify({ a: 2 }, null, 3));
+  let data = JSON.stringify({ a: 2 }, null, 3);
+  fs.writeFile("public/files/data.json", data, (err) => {
+    if (err)
+      console.log(err);
+    else {
+      console.log("File written successfully\n");
+    }
+  });
 });
 
-router.post('/', function(req, res, next) {  
-  res.setHeader('Content-Type', 'application/json');
-  res.end(req.body);
+router.post('/', function (req, res, next) {
+  let data = JSON.stringify(req.body);
+  fs.writeFile("public/files/data.json", data, (err) => {
+    if (err)
+      console.log(err);
+    else {
+      console.log("File written successfully\n");
+    }
+  });
 });
 
 
